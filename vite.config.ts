cec43bd40056843b6c171c33b33e2e1900619387
lemenezes <lmiglio@thoughtworks.com>
2026-05-21 12:06:49 -0300

    build: adiciona sitemap.xml automático com vite-plugin-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,34 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import ViteSitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    ViteSitemap({
+      hostname: 'https://www.consudes.com',
+      dynamicRoutes: [
+        '/',
+        '/galeria',
+        '/noticias',
+        '/federacoes',
+        '/esportes',
+        '/calendario',
+        '/interclubes',
+        '/contato',
+        '/historia',
+        '/missao',
+        '/valores',
+        '/equipe',
+        '/ex-presidentes',
+        '/relatorios',
+        '/admin/login',
+      ],
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
