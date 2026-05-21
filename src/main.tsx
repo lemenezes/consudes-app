37b2e67762611f372569d6ffe20d859993366d06
lemenezes <lmiglio@thoughtworks.com>
2026-05-21 10:39:23 -0300

    Add Google Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from "react-ga4";
 import './index.css'
 import App from './App.tsx'
 
+ReactGA.initialize("G-LR5VPGLW76");
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
